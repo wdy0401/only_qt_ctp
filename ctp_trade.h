d581053fa75c3f9875aa0e85e04f5a9c29e64884
wdy0401 <wdy0401@gmail.com>
2014-11-13 17:26:22 +0800

    add trade raw

--- ctp_trade.h
+++ ctp_trade.h
@@ -1,10 +1,48 @@
 #ifndef CTP_TRADE_H
 #define CTP_TRADE_H
 
-class ctp_trade
+#include<./ctp/ThostFtdcTraderApi.h>
+class ctp_trade:public
 {
 public:
-    ctp_trade();
+    virtual void OnFrontConnected();
+    virtual void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin,	CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    virtual void OnRspSettlementInfoConfirm(CThostFtdcSettlementInfoConfirmField *pSettlementInfoConfirm, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    virtual void OnRspQryInstrument(CThostFtdcInstrumentField *pInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    virtual void OnRspQryOrder(CThostFtdcOrderField *pOrder, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    virtual void OnRspQryTradingAccount(CThostFtdcTradingAccountField *pTradingAccount, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    virtual void OnRspQryInvestorPosition(CThostFtdcInvestorPositionField *pInvestorPosition, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    virtual void OnRspOrderInsert(CThostFtdcInputOrderField *pInputOrder, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    virtual void OnRspOrderAction(CThostFtdcInputOrderActionField *pInputOrderAction, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    virtual void OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+    virtual void OnFrontDisconnected(int nReason);
+    virtual void OnHeartBeatWarning(int nTimeLapse);
+    virtual void OnRtnOrder(CThostFtdcOrderField *pOrder);
+    virtual void OnRtnTrade(CThostFtdcTradeField *pTrade);
+
+    //
+    void cmd();
+    //报单查询请求
+    void ReqQryOrder();
+    //报单查询
+    void QryOrder();
+    // 是否收到成功的响应
+    bool IsErrorRspInfo(CThostFtdcRspInfoField *pRspInfo);
+    // 是否我的报单回报
+    bool IsMyOrder(CThostFtdcOrderField *pOrder);
+    // 是否正在交易的报单
+    bool IsTradingOrder(CThostFtdcOrderField *pOrder);
+
+
+private:
+    void ReqUserLogin();
+    void ReqSettlementInfoConfirm();
+    void ReqQryInstrument();
+    void ReqQryTradingAccount();
+    void ReqQryInvestorPosition();
+    void ReqOrderInsert();
+    void ReqOrderAction(CThostFtdcOrderField *pOrder);
+
 };
 
 #endif // CTP_TRADE_H
